f71e0cdc5cb9d4cbe3ad78d7c6812ae41c44e95f
Isaac Peña <isaac.pr99@gmail.com>
2021-06-13 02:27:20 -0400

    feat: extend configuration for chraka ui

--- theme.tsx
+++ theme.tsx
@@ -1,7 +1,16 @@
-import { extendTheme } from "@chakra-ui/react";
+import { ColorMode, extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
+interface ChakraConfig {
+  initialColorMode: ColorMode;
+  useSystemColorMode: boolean;
+}
 
 const fonts = { mono: `'Menlo', monospace` };
+
+const config: ChakraConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false
+};
 
 const breakpoints = createBreakpoints({
   sm: "40em",
@@ -11,6 +20,7 @@ const breakpoints = createBreakpoints({
 });
 
 const theme = extendTheme({
+  config,
   colors: {
     black: "#16161D"
   },
