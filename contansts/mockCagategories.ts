00aa6b9374df52e950a3fbd82685c6dbe18d849a
Isaac Peña <isaac.pr99@gmail.com>
2021-06-19 21:57:40 -0400

    feat: :clown_face: add fake data for testing menu components

---
+++ contansts/mockCagategories.ts
@@ -0,0 +1,106 @@
+export const mockCategories = [
+  {
+    sectionTitle: "Computación",
+    categories: [
+      {
+        path: "",
+        name: "Monitores"
+      },
+      {
+        path: "",
+        name: "Periféricos de PC"
+      },
+      {
+        path: "",
+        name: "Tablets"
+      },
+      {
+        path: "",
+        name: "Almacenamiento"
+      },
+      {
+        path: "",
+        name: "Procesadores"
+      },
+      {
+        path: "",
+        name: "GPUs"
+      }
+    ]
+  },
+  {
+    sectionTitle: "Deportes y Fitness",
+    categories: [
+      {
+        path: "",
+        name: "Ropa Deportiva"
+      },
+      {
+        path: "",
+        name: "Futbol"
+      },
+      {
+        path: "",
+        name: "Artes Marciales"
+      },
+      {
+        path: "",
+        name: "Ciclismo"
+      },
+      {
+        path: "",
+        name: "Natación"
+      }
+    ]
+  },
+  {
+    sectionTitle: "Electrodomésticos",
+    categories: [
+      {
+        path: "",
+        name: "Coccion"
+      },
+      {
+        path: "",
+        name: "Lavado"
+      },
+      {
+        path: "",
+        name: "Refrigeración"
+      },
+      {
+        path: "",
+        name: "Clima"
+      }
+    ]
+  },
+  {
+    sectionTitle: "Ropa, zapatos y accesorios",
+    categories: [
+      {
+        path: "",
+        name: "Abrigos"
+      },
+      {
+        path: "",
+        name: "Blusas"
+      },
+      {
+        path: "",
+        name: "Calzado"
+      },
+      {
+        path: "",
+        name: "Camisas"
+      },
+      {
+        path: "",
+        name: "Faldas"
+      },
+      {
+        path: "",
+        name: "Leggins"
+      }
+    ]
+  }
+];
