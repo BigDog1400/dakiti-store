429b5dc4362d50f0d3849b0ecc385e09274dca83
Isaac Peña <isaac.pr99@gmail.com>
2021-07-25 00:46:56 -0400

    feat: load more fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class Document extends NextDocument {
         <Head>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;300;400;500&family=Roboto&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;300;400;500&family=Roboto:wght@100;300;400;500&display=swap'
             rel='stylesheet'
           />
         </Head>
