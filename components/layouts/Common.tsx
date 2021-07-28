6564ef36db100eac93684e6bd46756c42b17750e
Isaac Peña <isaac.pr99@gmail.com>
2021-07-28 01:37:43 -0400

    feat: add maxW as a prop to <CommotLayout/>

--- components/layouts/Common.tsx
+++ components/layouts/Common.tsx
@@ -1,16 +1,18 @@
 import { Box, Container } from "@chakra-ui/layout";
+import { ContainerProps } from "@chakra-ui/react";
 import React from "react";
 import { Header } from "../elements/Header";
 
 type CommonLayoutProps = {
   children: React.ReactNode;
+  maxW?: ContainerProps["maxW"];
 };
 
-function CommonLayout({ children }: CommonLayoutProps) {
+function CommonLayout({ maxW = "container.xl", children }: CommonLayoutProps) {
   return (
     <Box>
       <Header />
-      <Container as='main' maxW='container.xl'>
+      <Container as='main' maxW={maxW}>
         {children}
       </Container>
     </Box>
