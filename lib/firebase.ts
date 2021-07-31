0f8c49b8cb33adb9bc42016bbae1dddf7aee52d1
Isaac Peña <isaac.pr99@gmail.com>
2021-07-31 11:11:08 -0400

    feat: add firebase initial setup file

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVNU60ybbkVpGraryrreZK-A-VRVE852Q",
+  authDomain: "dakiti-ecommerce.firebaseapp.com",
+  projectId: "dakiti-ecommerce",
+  storageBucket: "dakiti-ecommerce.appspot.com",
+  messagingSenderId: "314675585564",
+  appId: "1:314675585564:web:e4dbe2194ee344351df5d9"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
