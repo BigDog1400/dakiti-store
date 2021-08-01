3084b6b280ddcef6dbabd7a65d2e55266fe38ff1
Isaac Peña <isaac.pr99@gmail.com>
2021-08-01 02:30:25 -0400

    feat: add UserContext.Provider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
-
+import { UserContext } from "../contexts/authentication";
+import useUserData from "../hooks/useUserData";
 function MyApp({ Component, pageProps }: AppProps) {
+  const userData = useUserData();
+
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <UserContext.Provider value={userData}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserContext.Provider>
   );
 }
 
