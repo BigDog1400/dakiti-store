1ed7c54523565d60ab8f586c7db0bbc63b91cdc6
Isaac Peña <isaac.pr99@gmail.com>
2022-04-30 10:24:47 -0400

    fix

--- components/elements/ItemForSale.tsx
+++ components/elements/ItemForSale.tsx
@@ -4,32 +4,26 @@ import { StoreItem } from "../../models/storeItem";
 
 interface ItemForSaleProps extends StoreItem {}
 
-function ItemForSale({
-  description,
-  id,
-  image,
-  name,
-  price
-}: ItemForSaleProps) {
+function ItemForSale({ image, name, price }: ItemForSaleProps) {
   return (
     <Stack
-      overflow='hidden'
-      boxShadow='sm'
-      width='240px'
-      minWidth='240px'
+      overflow="hidden"
+      boxShadow="sm"
+      width="240px"
+      minWidth="240px"
       _hover={{
         boxShadow: "md",
-        cursor: "pointer"
+        cursor: "pointer",
       }}
     >
-      <Box width='100%'>
-        <Image objectFit='cover' width='100%' src={image}></Image>
+      <Box width="100%">
+        <Image objectFit="cover" width="100%" src={image}></Image>
       </Box>
-      <Stack spacing='0' padding={3} alignSelf='flex-start'>
-        <Text as='span' fontFamily='fireSans' fontSize='1.1rem'>
+      <Stack spacing="0" padding={3} alignSelf="flex-start">
+        <Text as="span" fontFamily="fireSans" fontSize="1.1rem">
           USDT {price}
         </Text>
-        <Text as='span'>{name} </Text>
+        <Text as="span">{name} </Text>
       </Stack>
     </Stack>
   );
